c355f745f45155772222ca4b8a0b8f5e694c3448
GMM2003 <50250400+GMM2003@users.noreply.github.com>
2021-09-06 20:41:10 -0400

    v1.1.0.0-beta4

--- VisExtBootChanger/Program.cs
+++ VisExtBootChanger/Program.cs
@@ -15,7 +15,7 @@ namespace VisExtBootChanger
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new StartCheck());
         }
     }
 }
